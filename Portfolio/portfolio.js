fcb9b5471f24fa565cf82b69f93c4afc71a48dd5
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-08-20 16:22:14 +0000

    updated Portfolio

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -25,7 +25,12 @@ const navLinkAction=()=>{
 navLink.forEach(n=>n.addEventListener('click',LinkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader=()=>{
+    const header=document.getElementById('header')
+    this.scrollY>=50 ? header.classList.add('blur-header')
+                     : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
 
 /*=============== EMAIL JS ===============*/
 

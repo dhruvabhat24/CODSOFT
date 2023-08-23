9705a0079508e589f0f4181c1c5a3d4694eee32b
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-08-23 10:22:23 +0000

    updated js

--- Portfolio/assets/js/portfolio.js
+++ Portfolio/assets/js/portfolio.js
@@ -97,5 +97,8 @@ const sr= ScrollReveal({
     delay: 400.
    // reset: true // Animation repeat
 })
-sr.reveal('.home__data, .home__social')
-sr.reveal('.home__data, .home__social')
+sr.reveal('.home__data, .home__social, .contact__container, .footer__container')
+sr.reveal('.home__image', {origin:'bottom'})
+sr.reveal('.about__data, .skills__data', {origin:'left'})
+sr.reveal('.about__image, .skills__content', {origin:'right'})
+sr.reveal('.projects__card', {interval:100})
